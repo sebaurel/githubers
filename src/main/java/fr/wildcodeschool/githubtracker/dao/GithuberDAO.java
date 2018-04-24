7a26622b57aee34e9cdb25aab2f0ee9f41365233
sebastien Aurel <sebaurel@free.fr>
2018-04-24 22:13:03 +0200

    Revert "Quete cdi"

--- src/main/java/fr/wildcodeschool/githubtracker/dao/GithuberDAO.java
+++
@@ -1,8 +0,0 @@
-package fr.wildcodeschool.githubtracker.dao;
-
-import fr.wildcodeschool.githubtracker.model.Githuber;
-import java.util.List;
-
-public interface GithuberDAO {
-    List<Githuber> getGithubers();
-}
